867cd61c75bc322b4e915fda12ea21f6b3d69992
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-02 12:45:07 -0400

    Triple check

--- content-script.js
+++ content-script.js
@@ -2,14 +2,14 @@ var clickedEl;
 
 document.addEventListener('mousedown', function(event){
   // Get the element tha was right clicked on.
-  if (event.button == 2) {
+  if (event.button === 2) {
       clickedEl = event.target;
   }
 }, true);
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request == 'remove') {
-      clickedEl.style.display = 'none';
-    }
+  if (request == 'remove') {
+    clickedEl.style.display = 'none';
+  }
 });
